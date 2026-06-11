815a5e0761f8e8d3286c94d4631f67d3f46a6fe7
Nitul Sonowal <nitulsonowal8133@gmail.com>
2026-06-11 16:39:02 +0530

    index change

--- server/index.js
+++ server/index.js
@@ -3,7 +3,19 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())  
+// app.use(cors())  
+app.use(
+  cors({
+    origin: [
+      'https://mediconnect-vert-rho.vercel.app',
+      'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
+app.options('*', cors());
 require('dotenv').config()
 
 
